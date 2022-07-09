c2d15f82e54113c2d8e2eeab513b1a7478f392f6
Pieter Pretorius <pieterpretorius223@gmail.com>
2022-07-09 13:18:01 +0200

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+
+const About = () => {
+	return (
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sit officia perferendis accusamus esse
+					reprehenderit obcaecati saepe quia tenetur! Cumque voluptates quam necessitatibus eveniet commodi ea in
+					laudantium nemo repellendus.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sit officia perferendis accusamus esse
+					reprehenderit obcaecati saepe quia tenetur! Cumque voluptates quam necessitatibus eveniet commodi ea in
+					laudantium nemo repellendus.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sit officia perferendis accusamus esse
+					reprehenderit obcaecati saepe quia tenetur! Cumque voluptates quam necessitatibus eveniet commodi ea in
+					laudantium nemo repellendus.
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default About;
